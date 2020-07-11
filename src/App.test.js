869ee98207bf1fb6086741027c5a44ad4a441498
whynaa <whynaagustin@gmail.com>
2020-07-11 13:44:08 +0700

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { renderer } from 'react-test-renderer';
 import App from './App';
+import {Navbar, MyHome, MyGold, MyContact, AboutMe, Footer} from './views'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+//saya tidak mengerti
+test('cliked myHome', () => {
+  const component = renderer.create(
+    <MyHome />
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshoot()
+
+  component.getInstance().clicked()
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshoot()
+})
